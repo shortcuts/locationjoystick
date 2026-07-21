3685014c7288b63e3c52053def697a1649f6d53c
shortcuts <vannicattec@gmail.com>
2026-07-21 15:06:56 +0200

    chore: remove unused throttleLatest() utility

--- core/common/src/main/kotlin/com/locationjoystick/core/common/util/FlowUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.locationjoystick.core.common.util
-
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.conflate
-import kotlinx.coroutines.flow.onEach
-
-fun <T> Flow<T>.throttleLatest(periodMs: Long): Flow<T> = conflate().onEach { delay(periodMs) }
